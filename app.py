f7bc2c53375e82bfaad0c2969195186bb835718a
Devlin Kros <devlin.kros.dk@gmail.com>
2024-07-23 13:27:56 +0200

    tussentijdse test

--- app.py
+++ app.py
@@ -28,49 +28,49 @@ def index():
 def send_dm():
     if not SLACK_TOKEN or not VERIFICATION_TOKEN:
         return jsonify({"error": "SLACK_TOKEN or VERIFICATION_TOKEN is not set in the environment"}), 500
-
     data = request.form
     token = data.get('token')
     user_id = data.get('user_id')
+    return jsonify({data}), 200
+
+    # if token != VERIFICATION_TOKEN:
+    #     return jsonify({"error": "Invalid request token"}), 403
+
+    # if user_id not in AUTHORIZED_USERS:
+    #     return jsonify({"error": "Unauthorized user"}), 403
+
+    # channel_id = data.get('channel_id')
+    # if not channel_id:
+    #     return jsonify({"error": "channel_id is required"}), 400
     
-    if token != VERIFICATION_TOKEN:
-        return jsonify({"error": "Invalid request token"}), 403
+    # headers = {
+    #     'Authorization': f'Bearer {SLACK_TOKEN}',
+    #     'Content-Type': 'application/json'
+    # }
 
-    if user_id not in AUTHORIZED_USERS:
-        return jsonify({"error": "Unauthorized user"}), 403
+    # payload_open_conversation = {
+    #     'users': user_id
+    # }
+    # response = requests.post('https://slack.com/api/conversations.open', headers=headers, json=payload_open_conversation)
 
-    channel_id = data.get('channel_id')
-    if not channel_id:
-        return jsonify({"error": "channel_id is required"}), 400
-    
-    headers = {
-        'Authorization': f'Bearer {SLACK_TOKEN}',
-        'Content-Type': 'application/json'
-    }
+    # if response.status_code != 200 or not response.json().get('ok'):
+    #     error_message = response.json().get('error', 'Unknown error') if response.status_code == 200 else "Failed to open a direct message channel"
+    #     return jsonify({"error": error_message}), response.status_code
 
-    payload_open_conversation = {
-        'users': user_id
-    }
-    response = requests.post('https://slack.com/api/conversations.open', headers=headers, json=payload_open_conversation)
+    # channel_id_personal = response.json()['channel']['id']
+    # verification = generate_token(user_id)
 
-    if response.status_code != 200 or not response.json().get('ok'):
-        error_message = response.json().get('error', 'Unknown error') if response.status_code == 200 else "Failed to open a direct message channel"
-        return jsonify({"error": error_message}), response.status_code
+    # payload_message = {
+    #     'channel': channel_id_personal,
+    #     'text': f"https://slack-activity-timeline.onrender.com/timeline/{channel_id}?verification={verification}"
+    # }
+    # response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, json=payload_message)
 
-    channel_id_personal = response.json()['channel']['id']
-    verification = generate_token(user_id)
+    # if response.status_code != 200 or not response.json().get('ok'):
+    #     error_message = response.json().get('error', 'Unknown error') if response.status_code == 200 else "Failed to send message"
+    #     return jsonify({"error": error_message}), response.status_code
 
-    payload_message = {
-        'channel': channel_id_personal,
-        'text': f"https://slack-activity-timeline.onrender.com/timeline/{channel_id}?verification={verification}"
-    }
-    response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, json=payload_message)
-
-    if response.status_code != 200 or not response.json().get('ok'):
-        error_message = response.json().get('error', 'Unknown error') if response.status_code == 200 else "Failed to send message"
-        return jsonify({"error": error_message}), response.status_code
-
-    return jsonify({"message": "success"}), 200
+    # return jsonify({"message": "success"}), 200
 
 @app.route('/timeline/<channel>')
 def get_history(channel):
